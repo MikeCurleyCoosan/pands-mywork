6f8c3c25a0ea29537c1c7c4b4be24d6dad0cafc2
Mike_Curley <michaelcurley1@gmail.com>
2024-02-07 16:02:15 +0000

    Added output.py

---
+++ Week3-work/Lab3.3.2.output.py
@@ -0,0 +1,9 @@
+#Simple program using tabs and newlines on Strings to change the way they are displayed
+
+#Author: Michael Curley
+
+message = 'John said "Hi" I said "Bye"'
+print(message)
+
+modifiedMessage ='John said \t"Hi"\nI said\t\t"Bye"'
+print(modifiedMessage)
