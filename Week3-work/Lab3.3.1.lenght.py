d6ac2f4b531bdadab936e1e2602f7e117632bb86
Mike_Curley <michaelcurley1@gmail.com>
2024-02-07 00:21:56 +0000

    Adding length.py

---
+++ Week3-work/Lab3.3.1.lenght.py
@@ -0,0 +1,9 @@
+# This program reads in a String and outputs its length
+# Author: Michael Curley
+
+inputString = str(input("Please enter a string: "))
+
+print(type(inputString))
+
+lenghtOfString = len(inputString)
+print('The lenght of {} is {}'.format(inputString,lenghtOfString))
