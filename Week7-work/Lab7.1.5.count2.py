fbb4cdf0473b5a16abbc3c755667f5fac818bf1c
Mike_Curley <michaelcurley1@gmail.com>
2024-03-06 14:48:58 +0000

    Adding Lab7.1.5.count2.py

---
+++ Week7-work/Lab7.1.5.count2.py
@@ -0,0 +1,28 @@
+#Write a program that counts how many times it was run
+#We will store the count in a file called count.txt. We are storing the count in a file so that it is not lost when the program ends.
+#We would normally use a database for this kind of thing, but that is a more advanced topic.
+
+#Author: Michael Curley
+#Date: 06/03/2024
+
+FILENAME = "count.txt" #The name of the file we will store the count in
+
+
+#Write a funttion that reads in a number from a file that already exists (count.txt)
+def read_number():
+    with open(FILENAME) as f: #Open the file for reading
+            number = int(f.read()) #Read the number from the file
+            return number #Return the number
+
+
+#Write a function that takes in a number and overwrites a file (count.txt) with the number.
+def write_number(number):
+    with open(FILENAME, "wt") as f:
+         #Write takes a string so we need to convert the number to a string
+         f.write(str(number)) #Write the number to the file
+
+#Main
+num = read_number() #Read the number from the file
+num += 1 #Increment the number
+print(f"We have run this program {num} times") #Print the number
+write_number(num) #Write the number to the file
