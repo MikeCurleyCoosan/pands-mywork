1d0ee34254f8b0a5694fe13d6fc1a942f979a88c
Mike_Curley <michaelcurley1@gmail.com>
2024-03-06 14:09:18 +0000

    Adding Lab7.1.2.test_b.py

--- Week7-work/Lab7.1.2.test_b.py
+++ Week7-work/Lab7.1.2.test_b.py
@@ -3,5 +3,9 @@
 # Date: 06/03/2024
 
 with open ("test_b.txt", "w") as f:
-    data = f.write("test b\n")
+    data = f.write("test b\n") #Return the number of characters written to the file
     print(data)
+
+with open ("test_b.txt", "w") as f2: #Open the file again. The file is overwritten
+    data = f2.write("another line\n") #Again, return the number of characters written to the file
+    print(data )
