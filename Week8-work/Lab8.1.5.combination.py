dc9f73336576a1e473f6f86b9108963b729ad072
Michael Curley <michaelcurley1@gmail.com>
2024-03-13 23:18:53 +0000

    Adding Lab8.1.5.combination.py to pands-mywork

--- Week8-work/Lab8.1.5.combination.py
+++ Week8-work/Lab8.1.5.combination.py
@@ -1,6 +1,4 @@
-# Write a program that makes a list called ages that has, the same number random
-#values as salaries, (range:21 to 65) . Make scatter plot of this data.
-
+#Have two differnt plots in the same figure
 #Author: Michael Curley
 #Date: 25/03/2019
 
@@ -20,11 +18,17 @@ salaries = np.random.randint(low_salary, high_salary, number_of_salaries)
 ages = np.random.randint(low=21, high=65, size = number_of_salaries) #Not a good idea, better to have absolute values for low and high created as variables
 
 #Create a scatter plot of the data
-plt.scatter(ages, salaries, color='r', label = "Salaries") #Create a scatter plot of the data. This will be random
+plt.scatter(ages, salaries, color='b', label = "Salaries") #Create a scatter plot of the data. This will be random
 plt.xlabel("Ages") #Label the x-axis
 plt.ylabel("Salaries") #Label the y-axis
 plt.title("Random Salaries and Ages") #Title the plot
 plt.legend() #Show the legend
+
+#Add x_squared
+x_points = np.array(range(1,101))
+y_points = x_points * x_points
+
+plt.plot(x_points, y_points, color='r', label = "x^2") #Create a scatter plot of the data. This will be random
+plt.legend() #Show the legend
+
 plt.show() #Show the plot
-
-
